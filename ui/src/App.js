d8130488e3824b45164d3fd3188d7f316a58baf4
undancer <undancer@gmail.com>
2018-09-29 11:36:57 +0800

    测试二者之间能否沟通

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    handleClick = () => {
+        console.log("click me");
+        fetch("/foo").then(value => {
+            console.log("get");
+            console.log(value);
+        });
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <button onClick={this.handleClick}>按钮</button>
+            </div>
+        );
+    }
 }
 
 export default App;

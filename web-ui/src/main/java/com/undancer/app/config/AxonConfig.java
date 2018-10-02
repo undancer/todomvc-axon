764d3056eee272dc9c93204aa4e1edbdc62e3654
undancer <undancer@gmail.com>
2018-10-02 16:40:02 +0800

    axon配置

--- web-ui/src/main/java/com/undancer/app/config/AxonConfig.java
+++ web-ui/src/main/java/com/undancer/app/config/AxonConfig.java
@@ -1,9 +1,33 @@
 package com.undancer.app.config;
 
+import org.axonframework.common.jpa.ContainerManagedEntityManagerProvider;
+import org.axonframework.common.jpa.EntityManagerProvider;
+import org.axonframework.common.transaction.TransactionManager;
+import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.jpa.JpaEventStorageEngine;
+import org.axonframework.spring.messaging.unitofwork.SpringTransactionManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 public class AxonConfig {
 
+    @Autowired
+    PlatformTransactionManager transactionManager;
+
+    @Bean
+    public EventStorageEngine eventStorageEngineJpa() {
+        return new JpaEventStorageEngine(entityManagerProvider(), transactionManager());
+    }
+
+    public EntityManagerProvider entityManagerProvider() {
+        return new ContainerManagedEntityManagerProvider();
+    }
+
+    public TransactionManager transactionManager() {
+        return new SpringTransactionManager(transactionManager);
+    }
 
 }

c00aaa95122eb93f09e53c5d2cf54f75cc68e44f
undancer <undancer@gmail.com>
2018-10-02 17:47:37 +0800

    领域相关对象独立为一个项目

--- data/src/main/kotlin/com/undancer/app/entity/jpa/TodoJpaRepository.kt
+++ data/src/main/kotlin/com/undancer/app/entity/jpa/TodoJpaRepository.kt
@@ -3,7 +3,6 @@ package com.undancer.app.entity.jpa
 import com.undancer.app.entity.TodoEntity
 import org.springframework.data.jpa.repository.JpaRepository
 
-
 interface TodoJpaRepository : JpaRepository<TodoEntity, String> {
 
     fun findTodoById(id: String): TodoEntity
---
+++ domain/src/main/kotlin/com/undancer/app/domain/Todo.kt
@@ -0,0 +1,68 @@
+package com.undancer.app.domain
+
+import com.undancer.app.commands.ChangeTodoTitleCommand
+import com.undancer.app.commands.CreateTodoCommand
+import com.undancer.app.commands.DeleteTodoCommand
+import com.undancer.app.evnets.ChangedTodoTitleEvent
+import com.undancer.app.evnets.CreatedTodoEvent
+import com.undancer.app.evnets.DeletedTodoEvent
+import com.undancer.app.values.TodoId
+import org.apache.commons.lang3.builder.ToStringBuilder
+import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.commandhandling.model.AggregateIdentifier
+import org.axonframework.commandhandling.model.AggregateLifecycle.apply
+import org.axonframework.eventsourcing.EventSourcingHandler
+import org.axonframework.spring.stereotype.Aggregate
+
+@Aggregate
+class Todo {
+
+    @AggregateIdentifier
+    var id: TodoId? = null
+
+    var title: String? = null
+
+    val completed: Boolean = false
+
+    constructor() {
+    }
+
+    @CommandHandler
+    constructor(command: CreateTodoCommand) {
+        //        apply(new CreatedTodoEvent(command.getId(), command.getTitle()), MetaData.with("name", "undancer"));
+        apply(CreatedTodoEvent(command.id, command.title))
+    }
+
+    @CommandHandler
+    fun on(command: ChangeTodoTitleCommand) {
+        println(this)
+        println(command)
+        apply(ChangedTodoTitleEvent(id!!, command.title))
+    }
+
+    @CommandHandler
+    fun on(command: DeleteTodoCommand) {
+        apply(DeletedTodoEvent(command.id))
+    }
+
+    @EventSourcingHandler
+    fun on(event: CreatedTodoEvent) {
+        this.id = event.id
+        this.title = event.title
+    }
+
+    @EventSourcingHandler
+    fun on(event: ChangedTodoTitleEvent) {
+        println("event -> $event")
+        this.title = title
+    }
+
+    @EventSourcingHandler
+    fun on(event: DeletedTodoEvent) {
+        println("删除聚合根？？？")
+    }
+
+    override fun toString(): String {
+        return ToStringBuilder.reflectionToString(this)
+    }
+}

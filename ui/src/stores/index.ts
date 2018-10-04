f19f1d79380d203c62968070e22344bacfa4ce79
undancer <undancer@gmail.com>
2018-10-04 20:51:58 +0800

    配置中间件

--- ui/src/stores/index.ts
+++ ui/src/stores/index.ts
@@ -1,6 +1,16 @@
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import reducer from '../reducers';
+import thunk from "redux-thunk";
+import {createLogger} from "redux-logger";
 
-const store = createStore(reducer);
+
+const logger = createLogger({
+    // ...options
+});
+
+const store = createStore(
+    reducer,
+    applyMiddleware(thunk, logger),
+);
 
 export {store}

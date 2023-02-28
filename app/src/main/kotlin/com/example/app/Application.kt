f6dbbc335d03c56ab2f40e753866ca797595cda1
undancer <undancer+github@gmail.com>
2023-02-28 17:35:17 +0800

    暂存临时项目

---
+++ app/src/main/kotlin/com/example/app/Application.kt
@@ -0,0 +1,14 @@
+package com.example.app
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application {
+}
+
+fun main(vararg args: String) {
+    runApplication<Application>(*args) {
+
+    }
+}
